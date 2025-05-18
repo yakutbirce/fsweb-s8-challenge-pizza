8fcba92270e63c7c41d19d85eabdf710be889000
Birçe Yakut <yakutbirce@gmail.com>
2025-05-18 17:53:31 +0300

    fixed

--- src/App.jsx
+++ src/App.jsx
@@ -1,45 +1,19 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Routes, useLocation } from "react-router-dom";
-import { CSSTransition, TransitionGroup } from "react-transition-group";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Order from "./pages/Order";
 import Success from "./pages/Success";
 import HomePage from "./pages/HomePage";
 import Header from "./components/Header";
-import "./styles/Transitions.css"; 
-
-const AnimatedRoutes = () => {
-  const location = useLocation(); 
-  
-  return (
-    <TransitionGroup>
-      <CSSTransition key={location.pathname} classNames="fade" timeout={500}>
-        <Routes location={location}>
-          {/* Anasayfa */}
-          <Route path="/" element={<HomePage />} />
-
-          {/* Sipariş Sayfası */}
-          <Route
-            path="/order"
-            element={
-              <>
-                <Header />
-                <Order />
-              </>
-            }
-          />
-
-          {/* Success Sayfası */}
-          <Route path="/success" element={<Success />} />
-        </Routes>
-      </CSSTransition>
-    </TransitionGroup>
-  );
-};
 
 function App() {
   return (
     <Router>
-      <AnimatedRoutes />
+      <Header />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/order" element={<Order />} />
+        <Route path="/success" element={<Success />} />
+      </Routes>
     </Router>
   );
 }
